56628900a92105c7f687b6ef2e1efc716e47d4f9
Rafif Yusa <Rafif_RY276@mitrais.com>
2019-10-31 11:40:42 +0800

    First commit - chapter 20

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT into exchange_value(id, currency_from, currency_to, conversion_multiplier, port) values (10001, 'USD', 'INR', 65, 0)
+INSERT into exchange_value(id, currency_from, currency_to, conversion_multiplier, port) values (10002, 'EUR', 'INR', 75, 0)
+INSERT into exchange_value(id, currency_from, currency_to, conversion_multiplier, port) values (10003, 'AUD', 'INR', 25, 0)
